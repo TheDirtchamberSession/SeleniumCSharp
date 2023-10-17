89738808ec91f2f90c044939b44bfca8eab15c64
Srdjan <srdjan.sljanic@reprisk.com>
2023-10-17 09:03:16 +0200

    Update project 457

--- SeleniumProjectTest/Fun/FunLogIn.cs
+++ SeleniumProjectTest/Fun/FunLogIn.cs
@@ -6,13 +6,12 @@ using TestProject1.utility;
 
 namespace TestProject1.Fun;
 
-public class FunLogIn:BaseTestHub
+public class FunLogIn:BaseTest
 {
    
     public static void LogInAccount (WebDriver driver, WebDriverWait wait){
        
-      //  var envData = EnvReader.Load(".env");
-
+        //var envData = EnvReader.Load(".env");
         driver.FindElement(LogInPageObject.UserNameField).SendKeys(EnvData["USERNAME"]);
         driver.FindElement(LogInPageObject.UserPasswordField).SendKeys(EnvData["PASSWORD"]);
         driver.FindElement(LogInPageObject.LogInButton).Click();
--- SeleniumProjectTest/Tests/SimpleTest.cs
+++ SeleniumProjectTest/Tests/SimpleTest.cs
@@ -3,21 +3,21 @@ using TestProject1.utility;
 
 namespace TestProject1.Tests
 {
-    public class SimpleTest:BaseTestHub
+    public class SimpleTest : BaseTestHub
     {
+
+            [Test]
+            public void NavigateToGoogleAndCheckTitle()
+            {
+                driver.Navigate().GoToUrl("https://www.google.com");
+                Thread.Sleep(3000);
         
-        [Test]
-        public void NavigateToGoogleAndCheckTitle()
-        {
-            driver.Navigate().GoToUrl("https://www.google.com");
-            Thread.Sleep(3000);
-
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            driver.Quit();
+            }
+        
+            [TearDown]
+            public void TearDown()
+            {
+                driver.Quit();
+            }
         }
     }
-}
